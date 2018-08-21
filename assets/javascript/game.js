55a9a0ce757f230dcc813ce12a31587938eab4db
R-Delgado1203 <r.delgado1203@gmail.com>
2018-08-21 10:06:27 -0700

    completed JS/Jquery logic, updated UI

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,50 +1,119 @@
-var gem1 = 0;
-var gem2 = 0;
-var gem3 = 0;
-var gem4 = 0;
-
+var ruby = 0;
+var amber = 0;
+var saphire = 0;
+var emerald = 0;
 var targetScore = 0;
 var wins = 0;
-var loss = 0;
+var losses = 0;
 var currentScore = 0;
 
 function numGenerator() {
-    gem1 = Math.floor((Math.random() * 10) + 2);
-    gem2 = Math.floor((Math.random() * 10) + 2);
-    gem3 = Math.floor((Math.random() * 10) + 2);
-    gem4 = Math.floor((Math.random() * 10) + 2);
-
+    ruby = Math.floor((Math.random() * 10) + 2);
+    amber = Math.floor((Math.random() * 10) + 2);
+    saphire = Math.floor((Math.random() * 10) + 2);
+    emerald = Math.floor((Math.random() * 10) + 2);
     targetScore = Math.floor(((Math.random() * 50) + 13));
+    currentScore = 0;
+
+    //$("#footer-content").text("");
+    $(".target-score").text(targetScore);
+    $(".current-score").text(currentScore);
 }
 
 function addGems(value) {
-
     //take currentScore add gem value
     currentScore += value;
 }
 
+function crystalCollector(){
+    if (currentScore > targetScore){
+        losses++; 
+        //$("#footer-content").text("YOU LOSE!");
+        $(".losses").text(losses);
+        numGenerator();
+    }
+    else if (currentScore === targetScore){
+        wins++;
+        //$("#footer-content").text("YOU WIN!");
+        $(".wins").text(wins);
+        numGenerator();
+    }
+}
+
+
 numGenerator();
 
+$(document).ready(function () {
 
-$(".gem1").on("click", function () {
-    addGems(gem1);
-    $(".currentScore").text(currentScore);
-});
-$(".gem2").on("click", function () {
-    addGems(gem2);
-});
-$(".gem3").on("click", function () {
-    addGems(gem3);
-});
-$(".gem4").on("click", function () {
-    addGems(gem4);
-});
+/*     $(document).on("click", function(){
+        if (currentScore > targetScore){
+            losses++; 
+            $("#footer-content").text("YOU LOSE!");
+            $(".losses").text(losses);
+            numGenerator();
+        }
+        else if (currentScore === targetScore){
+            wins++;
+            $("#footer-content").text("YOU WIN!");
+            $(".wins").text(wins);
+            numGenerator();
+        }
+    }); */
 
 
 
 
+    $(".target-score").text(targetScore);
 
-console.log(targetScore);
+    $("#btn-ruby").on("click", function () {
+        addGems(ruby);
+        $(".current-score").text(currentScore);
+        crystalCollector();
+
+
+        console.log("the value of this gem is " + ruby);
+        console.log("new current score " + currentScore);
+        console.log("---------");
+        
+    });
+    $("#btn-amber").on("click", function () {
+        addGems(amber);
+        $(".current-score").text(currentScore);
+        crystalCollector();
+
+
+        console.log("the value of this gem is " + amber);
+        console.log("new current score " + currentScore);
+        console.log("---------");
+    });
+    $("#btn-saphire").on("click", function () {
+        addGems(saphire);
+        $(".current-score").text(currentScore);
+        crystalCollector();
+
+
+        console.log("the value of this gem is " + saphire);
+        console.log("new current score " + currentScore);
+        console.log("---------");
+    });
+    $("#btn-emerald").on("click", function () {
+        addGems(emerald);
+        $(".current-score").text(currentScore);
+        crystalCollector();
+
+
+        console.log("the value of this gem is " + emerald);
+        console.log("new current score" + currentScore);
+        console.log("---------");
+    });
+
+})
+
+
+
+console.log("Target score: " + targetScore);
+console.log("---------");
+
 /* console.log(gem1);
 console.log(gem2);
 console.log(gem3);
